8e42172c602dfbddcbc162b8a10751af8858c286
travywill <travywill@gmail.com>
2019-06-19 16:42:39 -0400

    Added recursive method for reversing Strings
    I'm still getting the hang of GitHub, so I manually uploaded the same file twice.  Sorry if there's two files or if anything else is weird as a result.

--- WeekB/src/com/revature/strings/Wednesday.java
+++ WeekB/src/com/revature/strings/Wednesday.java
@@ -3,11 +3,23 @@ package com.revature.strings;
 public class Wednesday {
 
 	public static void main (String[] args) {
-		String yourString = "Travis";
+		String yourString = "String";
 		String reversedString = reverseString(yourString);
 		System.out.println("Reverse of your String = " + reversedString);
 		System.out.println();
-		System.out.println("Is racecar a palindrome? A: " + isPalindrome("racecar"));
+		
+		String palindromeCheck = "racecar";
+		System.out.println("Is " + palindromeCheck + " a palindrome? A: " + isPalindrome(palindromeCheck));
+		System.out.println();
+		
+		int n = 5;
+		System.out.println("The factorial of " + n + " is: " + calculateFactorial(5));
+		System.out.println();
+		
+		String yourString2 = "String";
+		String recursiveReversedString = reverseStringUsingRecursion(yourString2);
+		System.out.println("Reverse of your String, using recursion = " + recursiveReversedString);
+		System.out.println();
 	}
 	
 	public static String reverseString (String str) {
@@ -35,5 +47,27 @@ public class Wednesday {
 			palindrome = true;
 		}
 		return palindrome;
+	}
+	
+	public static int calculateFactorial (int n) {
+		//
+		if (n == 0 || n == 1) {
+			return 1;
+		}
+		return n * calculateFactorial(n - 1);
+	}
+	
+	public static String reverseStringUsingRecursion (String str) {
+		String lastChar = str.substring(str.length()-1);
+		String firstChars = str.substring(0, str.length()-1);
+		
+		// base case
+		if (str.length() == 1) {
+			return str.substring(0);
+		}
+		
+		// recursive case
+		return lastChar + reverseStringUsingRecursion(firstChars);
+		
 	}
 }
