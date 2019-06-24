0fb8836f2632be048ab918fe23e6a3a59054ba96
travywill <travywill@gmail.com>
2019-06-24 13:43:49 -0400

    Updated MinimumMutations to allow file input

--- MinimumMutations/src/com/revature/minimummutations/Driver.java
+++ MinimumMutations/src/com/revature/minimummutations/Driver.java
@@ -1,21 +1,69 @@
 package com.revature.minimummutations;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Driver {
 
-	public static void main(String[] args) {
-		Utility minimumMutationChecker = new Utility();
-		
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		/*
 		// First attempt - hardcoding
-		String startingGene = "AACCGGTT";
-		//String startingGene = "AAACGGTA";
-		String endingGene = "AAACGGTA";
+		 * 
+		String startGene = "AACCGGTT";
+		//String startGene = "AAACGGTA";
+		String endGene = "AAACGGTA";
 		ArrayList<String> geneBank = new ArrayList<String>();
 		geneBank.add("AACCGGTA");
 		geneBank.add("AACCGCTA");
 		geneBank.add("AAACGGTA");
+		*/
 		
-		System.out.println("The minimum number of mutations for the gene " + startingGene + " to become " + endingGene + " is: " + minimumMutationChecker.minimumNumMutations(startingGene, endingGene, geneBank));
+		// Second attempt - reading input from a file
+		
+		/*
+		 * Here is where I created the text file:
+		 * 
+		// Creating objects to be written to a file
+		
+		String sGene = "AACCGGTT";
+		String eGene = "AAACGGTA";
+		ArrayList<String> gBank = new ArrayList<String>();
+		gBank.add("AACCGGTA");
+		gBank.add("AACCGCTA");
+		gBank.add("AAACGGTA");
+		
+		// Writing objects to a file
+		ObjectOutputStream objectOutput = new ObjectOutputStream(new FileOutputStream("MinimumMutations.txt"));
+		objectOutput.writeObject(sGene);
+		objectOutput.writeObject(eGene);
+		objectOutput.writeObject(gBank);
+		*/
+		
+		// Reading file and creating objects from it
+		ObjectInputStream objectInput = new ObjectInputStream(new FileInputStream("MinimumMutations.txt"));
+		
+		String startGene = (String) objectInput.readObject();
+		String endGene = (String) objectInput.readObject();
+		ArrayList<String> geneBank = (ArrayList<String>) objectInput.readObject();
+	    
+		/*
+		// Checking to see if it read the file and created objects correctly
+	    System.out.println(startGene);
+	    System.out.println(endGene);
+	    System.out.println(geneBank);
+	    */
+	    
+	    // Creating a Utility object and running the minimumNumMutations() method on it
+		// using the object created from reading the file
+		
+		Utility minimumMutationChecker = new Utility();
+		
+		System.out.println("The minimum number of mutations for the gene " + startGene + " to become " + endGene + " is: " + minimumMutationChecker.minimumNumMutations(startGene, endGene, geneBank));
 	}
 }
